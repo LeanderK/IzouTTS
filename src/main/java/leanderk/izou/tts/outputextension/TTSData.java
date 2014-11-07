5f87f3e65bd9498e078366e431bf7f0e65de225f
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2014-11-07 18:14:35 +0100

    created classes for OutputExtension and OutputPlugin + Helper Classes

--- src/main/java/leanderk/izou/tts/outputextension/TTSData.java
+++ src/main/java/leanderk/izou/tts/outputextension/TTSData.java
@@ -2,42 +2,35 @@ package leanderk.izou.tts.outputextension;
 
 /**
  * Holds the Data for TTS.
- * To get an Instance, use the factory method {@link #createTTSData(String, String, int, String)} or
- * {@link #createTTSData(String, String, int, String, String, String)}.<br><br>
- * It contains:
- * <ul>
- * <li>String words, which holds the message to be spoken
- * <li>int priority, which is used to place this particular message in a list of messages.<br>
+ * It contains:<br>
+ * - String words, which holds the message to be spoken<br>
+ * - int priority, which is used to place this particular message in a list of messages.<br>
  *      0 is max priority, maxInt least priority<br>
  *      AVOID 0. It is preserved for really important parts!
- * <li>String sourceID, which holds the ID where the message was generated
- * <li>String beforeID, you can use this to place your message before another message
- * <li>String afterID, you can use this to place your message after another message
- * </ul>
+ * - String sourceID, which holds the ID where the message was generated<br>
+ * - String beforeID, you can use this to place your message before another message<br>
+ * - String afterID, you can use this to place your message after another message<br>
  */
 @SuppressWarnings("UnusedDeclaration")
 public class TTSData {
     private String words;
-    private String locale;
     //0 most priority, maxInt least priority
     private int priority;
     private String sourceID;
     private String beforeID;
     private String afterID;
 
-    private TTSData(String words, String locale, int priority, String sourceID, String beforeID, String afterID) {
+
+    private TTSData(String words, int priority, String sourceID, String beforeID, String afterID) {
         this.words = words;
-        this.locale = locale;
         this.priority = Math.abs(priority);
         this.sourceID = sourceID;
         this.beforeID = beforeID;
         this.afterID = afterID;
     }
 
-
-    private TTSData(String words, String locale, int priority, String sourceID) {
+    private TTSData(String words, int priority, String sourceID) {
         this.words = words;
-        this.locale = locale;
         this.priority = priority;
         this.sourceID = sourceID;
     }
@@ -46,35 +39,22 @@ public class TTSData {
      * factory method for TTSData.
      * for further information about the parameters, see documentation of the class
      * @param words the message, may be null or empty
-     * @param locale the locale code, must not be null or empty
      * @param priority the priority
      * @param sourceID the sourceID, must not be null or empty
      * @param beforeID the beforeID, may be null or empty
      * @param afterID the afterID, may be null or empty
-     * @return TTSData an instance of null
+     * @return
      */
-    public static TTSData createTTSData(String words, String locale, int priority, String sourceID, String beforeID, String afterID) {
-        if(sourceID == null || sourceID.trim().isEmpty() || locale == null) return null;
-        return new TTSData(words, locale, priority, sourceID, beforeID, afterID);
+    public static TTSData createTTSData(String words, int priority, String sourceID, String beforeID, String afterID) {
+        if(sourceID == null || sourceID.trim().isEmpty()) return null;
+        return new TTSData(words, priority, sourceID, beforeID, afterID);
     }
 
-    /**
-     * factory method for TTSData.
-     * for further information about the parameters, see documentation of the class
-     * @param words the message, may be null or empty
-     * @param locale the locale code, must not be null or empty
-     * @param priority the priority
-     * @param sourceID the sourceID, must not be null or empty
-     * @return TTSData an instance of null
-     */
-    public static TTSData createTTSData(String words, String locale, int priority, String sourceID) {
-        if(sourceID == null || sourceID.trim().isEmpty() || locale == null) return null;
-        return new TTSData(words, locale, priority, sourceID);
+    public static TTSData createTTSData(String words, int priority, String sourceID) {
+        if(sourceID == null || sourceID.trim().isEmpty()) return null;
+        return new TTSData(words, priority, sourceID);
     }
 
-    public String getLocale() {
-        return locale;
-    }
 
     public String getWords() {
         return words;
