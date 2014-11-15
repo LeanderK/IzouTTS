509930c135f1f41d00457ea2952d5de91af3f6d1
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2014-11-15 18:18:19 +0100

    documentation....too much documentation

--- src/main/java/leanderk/izou/tts/outputplugin/TTSOutputPlugin.java
+++ src/main/java/leanderk/izou/tts/outputplugin/TTSOutputPlugin.java
@@ -2,26 +2,42 @@ package leanderk.izou.tts.outputplugin;
 
 import com.gtranslate.Audio;
 import com.gtranslate.context.TranslateEnvironment;
+import intellimate.izou.output.OutputExtension;
 import intellimate.izou.output.OutputPlugin;
 import leanderk.izou.tts.outputextension.TTSData;
+import leanderk.izou.tts.outputextension.TTSOutputExtension;
 
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
- * Created by LeanderK on 02/11/14.
+ * Output TTS Plugin
+ * TTSOutputExtensions generate the Sentences and this plugin is is responsible for speech synthesization.
  */
 public class TTSOutputPlugin extends OutputPlugin<TTSData>{
-    private TTSElementCollection collection;
-
+    @SuppressWarnings("WeakerAccess")
     public static final String ID = TTSOutputPlugin.class.getCanonicalName();
+    private final TTSElementCollection collection;
     private final int Buffer = 10;
-    private Audio audio;
+    private int currentBuffer = 0;
+    private final Audio audio;
+    @SuppressWarnings("UnusedDeclaration")
+    private ExecutorService executor = Executors.newFixedThreadPool(Buffer/2);
+    private String locale;
 
     public TTSOutputPlugin() {
+        this(null);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public TTSOutputPlugin(@SuppressWarnings("SameParameterValue") Properties properties) {
         super(ID);
-        collection = new TTSElementCollection();
+        collection = new TTSElementCollection(executor);
 
         //required values
         String enableProxy = "false";
@@ -30,7 +46,11 @@ public class TTSOutputPlugin extends OutputPlugin<TTSData>{
         String googleTranslateText = "http://translate.google.com.{locale}/translate_a/t?";
         String googleTranslateAudio = "http://translate.google.com/translate_tts?";
         String googleTranslateDetect = "http://www.google.com/uds/GlangDetect?";
-        String locale = "en";
+        if(properties != null && properties.getProperty("locale") != null) {
+            locale = properties.getProperty("locale");
+        } else {
+            locale = Locale.getDefault().getLanguage();
+        }
 
         //initializing
         TranslateEnvironment.init(enableProxy,
@@ -53,9 +73,35 @@ public class TTSOutputPlugin extends OutputPlugin<TTSData>{
         collection.clear();
         dataList.forEach(collection::addTTSElement);
         LinkedList<TTSElement> elements = collection.getFullCollectionAsList();
+        bufferAndSpeak(elements);
+    }
+
+    /**
+     * event is called when an output-extension is added to this output-plugin
+     *
+     * @param outputExtension the outputExtension that was added to the outputPlugin
+     */
+    @Override
+    public void outputExtensionWasAdded(OutputExtension<TTSData> outputExtension) {
+        if(TTSOutputExtension.class.isInstance(outputExtension)) {
+            TTSOutputExtension extension = (TTSOutputExtension)outputExtension;
+            extension.setLocale(locale);
+        }
     }
 
     private void bufferAndSpeak(LinkedList<TTSElement> elements) {
+        while(elements.size() > 0) {
+            if(currentBuffer < Buffer) {
+                elements.stream().filter(element -> !element.bufferingStarted()).forEach(
+                        element -> element.buffer(() -> currentBuffer--));
+            }
+            if(elements.get(0).bufferingFinished()) {
+                TTSElement element = elements.pop();
+                LinkedList<InputStream> inputStreams = element.getInputStreams();
+                inputStreams.forEach(this::speak);
+            }
+        }
+        currentBuffer = 0;
     }
 
     private void speak(InputStream sound) {
